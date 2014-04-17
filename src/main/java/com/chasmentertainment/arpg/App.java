dc4ce491e0e58ecfe2fd3aaf7176eabdd78c7381
tsjnsn <tsjnsn@gmail.com>
2014-04-17 10:51:56 -0500

    switched to using maven repository to manage dependencies and folder structure

---
+++ src/main/java/com/chasmentertainment/arpg/App.java
@@ -0,0 +1,13 @@
+package com.chasmentertainment.arpg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
